0d094b0f9811ab9442622af6bd76084ba869f29f
zboyyy10 <126742130+zboyyy10@users.noreply.github.com>
2023-03-14 10:27:31 +0800

    Update 1aaqiandao.js

--- 1aaqiandao.js
+++ 1aaqiandao.js
@@ -2,11 +2,21 @@
 //此代码只用于测试学习请24小时内进行删除!，请勿用于违法操作，产生一切后果由使用者承担全部责任!
 var bs = "Mozilla/5.0 (Linux; Android 6.0.1; OPPO R9s Plus Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36"
 var Token =false  
+var q66="https://ghproxy.com/https://github.com/zboyyy10/autoxx/blob/main/66.js"
+var q1aa="https://ghproxy.com/https://github.com/zboyyy10/autoxx/blob/main/1aaqiandao.js"
+var q2ee="https://ghproxy.com/https://github.com/zboyyy10/autoxx/blob/main/2eeqiandao.js"
+var q77k="https://ghproxy.com/https://github.com/zboyyy10/autoxx/blob/main/77kqiandao.js"
 ui.layout(
     <vertical gravity='center_vertical'>
         <img id='img1' w='200' h='200' layout_gravity='center_horizontal' />
         <text layout_gravity='center_horizontal' id='t2' w='auto' h='50'  textColor="red"  />
-        <button id='an1' style="Widget.AppCompat.Button.Colored" text="刷新二维码" />
+        <button id='an1' style="Widget.AppCompat.Button.Colored" text="1AA刷新二维码" />
+          <horizontal>
+        <button id='q1'w='90' style="Widget.AppCompat.Button.Colored" text="1aa" />
+        <button id='q2' w='90'style="Widget.AppCompat.Button.Colored" text="2ee" />
+        <button id='q3' w='90'style="Widget.AppCompat.Button.Colored" text="7kk" />
+        <button id='q4' w='*'style="Widget.AppCompat.Button.Colored" text="六合一" />
+        </horizontal>
         <ScrollView >
             <text gravity='center' id='t1'  w='*' h='*' textColor="red" textSize='30' />
         </ScrollView>
@@ -188,7 +198,22 @@ var gonmgneng = {
         if (s == 0) return p1
     }
 };
-
+ui.q1.on("click", () => {
+    toast("1aa")
+    var e1 = engines.execScript("1aa网址", http.get(q1aa).body.string()); 
+   });
+   ui.q2.on("click", () => {
+    toast("2ee")
+    var e2 = engines.execScript("2ee网址", http.get(q2ee).body.string());
+   });
+   ui.q3.on("click", () => {
+    toast("77k")
+    var e3  = engines.execScript("77k网址", http.get(q77k).body.string());
+   });
+   ui.q4.on("click", () => {
+    toast("主页")
+    var e4 = engines.execScript("666网址", http.get(q66).body.string());
+   });
 threads.start(function() {
     gonmgneng.rukou();
 })
